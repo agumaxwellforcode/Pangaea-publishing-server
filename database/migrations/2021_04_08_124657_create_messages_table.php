dba55dc278a9c396a6683d11817f8c6e8f6dd6ea
Agu Maxwell <agumaxwell9@gmail.com>
2021-04-08 14:04:33 +0100

    If added will setup models and migrations

---
+++ database/migrations/2021_04_08_124657_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->unsignedBigInteger('topic_id')->nullable();
+            $table->foreign('topic_id')
+                ->references('id')
+                ->on('topics')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
